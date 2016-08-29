4bcbdf95a72ba890c8fb0f93e522aa3924787329
Looker <dingt90@163.com>
2016-08-29 12:31:37 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/looker/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.looker.coolweather.model;
+
+/**
+ * Created by looker on 2016/8/29.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
